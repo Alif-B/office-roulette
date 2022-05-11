6effcaba9ab3d6e116af3397f2a682c749f6e9e8
Alif <ambam3110@gmail.com>
2022-05-10 23:12:11 -0700

    Frontend staging version done

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
-function bet(bettingId){
-    document.getElementsByClassName("bet").style.opacity = 1;
+// Default values for a bet
+var theBet = "no-bet";
+var theBettor = "unknown";
+
+function betted(bettedOn){
+    theBet = bettedOn
+
+    let clear = document.getElementsByClassName('bet');
+    for (let i=0; i<clear.length; i++)  {clear[i].style.opacity = 0};
+
+    document.getElementById(bettedOn).style.opacity = 1;
+}
+
+function submitBet(){
+    theBettor = document.getElementById("selection").value;
+    console.log(theBettor + " bet on " + theBet);
 }
